4e468da713c7a112e2faec4493715a37d3fdae75
shinya8818 <wacker8818@outlook.com>
2019-06-02 19:54:03 +0900

    profile_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+  get '/users/:id', to: 'users#show', as: 'user'
 
 end
