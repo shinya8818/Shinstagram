7d85662d1b6f704041ccf6f5ee76fb4bb1f2cda1
shinya8818 <wacker8818@outlook.com>
2019-06-02 21:01:54 +0900

    post_lists_view

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user'
 
-  resources :posts, only: [:new, :create] do
+  resources :posts, only: [:new, :create, :index] do
     resources :photos, only: [:create]
   end
 
