43a23e9c1cbbbc7c7553435d17e49b85c50d41ae
shinya8818 <wacker8818@outlook.com>
2019-06-03 15:31:41 +0900

    posts.controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+    @posts = Post.limit(3).includes(:photos, :user).order('created_at DESC')
   end
 
   def show
